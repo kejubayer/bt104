7eba8892774bd629856f8c1558f883772edba3b3
kejubayer <khondokereftakharjubayer@gmail.com>
2021-06-16 20:50:51 +0600

    relation

---
+++ database/migrations/2021_06_16_141302_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('order_no');
+            $table->string('price');
+            $table->string('qty');
+            $table->string('payment_method');
+            $table->string('txn_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
